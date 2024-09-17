d95f47780a0fe562fc173d68e323d1e2707ef9d3
Florian C <floriancartuta2@yahoo.com>
2024-09-17 18:31:27 +0300

    Flask API.

---
+++ schemas.py
@@ -0,0 +1,53 @@
+from marshmallow import Schema, fields
+
+
+class PlainItemSchema(Schema):
+    id = fields.Int(dump_only=True) # dump_only = doar to return data through API. Not used for incoming data validation
+    name = fields.Str(required=True)
+    price = fields.Float(requred=True)
+
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True) 
+    name = fields.Str(required=True)
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True) 
+    name = fields.Str(required=True)
+
+
+class ItemUpdateSchema(Schema):
+    name = fields.Str()
+    price = fields.Float()
+    store_id = fields.Int()
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True) # used only when returning data to the client
+    tags = fields.Nested(PlainTagSchema(), dump_only=True) # used only when returning data to the client
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+
+# class TagSchema(PlainTagSchema):
+#     store_id = fields.Int(load_only=True)
+#     store = fields.Nested(PlainStoreSchema(), dump_only=True) # used only when returning data to the client
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
+
+    
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    username = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
